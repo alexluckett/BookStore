537244ccd55b67fcce39cda6e00b927e5a64ca5c
Alex Luckett <alexgluckett@gmail.com>
2015-03-23 21:58:14 +0000

    Removed global state from ActionFactory by loading actions via a ClassLoader proxy

--- controller/ActionFactory.php
+++ controller/ActionFactory.php
@@ -1,13 +1,15 @@
 <?php
 
+include_once ('ClassLoader.php');
+
+include_once 'IAction.php';
+include_once 'model/actions/AuthenticatedAction.php';
+include_once 'model/actions/GuestAction.php';
+
 include_once 'model/dbaccess/UserDAO.php';
 include_once 'model/dbaccess/BasketDAO.php';
 include_once 'model/dbaccess/BookDAO.php';
 include_once 'model/dbaccess/CategoryDAO.php';
-
-include_once 'model/actions/IAction.php';
-include_once 'model/actions/AuthenticatedAction.php';
-include_once 'model/actions/GuestAction.php';
 
 include_once 'model/DomainModel.php';
 include_once 'model/BookCategoryModel.php';
@@ -71,31 +73,31 @@ class ActionFactory {
             "viewLogin" => new LoadPage("/view", "login.php"), // display login page
             "viewRegister" => new LoadPage("/view", "register.php"), // display account registration page
             "viewInfo" => new LoadPage("/view", "info.php"), // display information page
-            "viewEditUser" => new AddUserBalancePage(self::$staffPermission), // view edit user page
-            "viewAccountDetails" => new ViewAccountDetails(self::$userPermission), // view account details page
-            "viewBooks" => new ViewBookList(self::$userPermission), // view a list of books
-            "viewAddBook" => new AddBookForm(self::$staffPermission), // display page to add book
-            "viewBasket" => new ViewBasket(self::$userPermission), // view items in basket
-            "viewUserBasket" => new ViewBasketStaff(self::$staffPermission), // view user's basket (from outside account)
-            "viewUsers" => new ViewUsers(self::$staffPermission), // view a list of users
-            "viewAddQuantity" => new ViewAddQuantity(self::$staffPermission), // view page to add book quantity
-            "viewCategories" => new ViewCategoryList(self::$staffPermission), // view page to add book quantity
-            "viewAddCategory" => new ViewAddCategory(self::$staffPermission), // view page to add book quantity
+            "viewEditUser" => new ClassLoader("AddUserBalancePage", self::$staffPermission), // view edit user page
+            "viewAccountDetails" => new ClassLoader("ViewAccountDetails", self::$userPermission), // view account details page
+            "viewBooks" => new ClassLoader("ViewBookList", self::$userPermission), // view a list of books
+            "viewAddBook" => new ClassLoader("AddBookForm", self::$staffPermission), // display page to add book
+            "viewBasket" => new ClassLoader("ViewBasket", self::$userPermission), // view items in basket
+            "viewUserBasket" => new ClassLoader("ViewBasketStaff", self::$staffPermission), // view user's basket (from outside account)
+            "viewUsers" => new ClassLoader("ViewUsers", self::$staffPermission), // view a list of users
+            "viewAddQuantity" => new ClassLoader("ViewAddQuantity", self::$staffPermission), // view page to add book quantity
+            "viewCategories" => new ClassLoader("ViewCategoryList", self::$staffPermission), // view page to add book quantity
+            "viewAddCategory" => new ClassLoader("ViewAddCategory", self::$staffPermission), // view page to add book quantity
             
             /* functions */
             "register" => new CreateAccount(), // log in to system
             "login" => new UserLogin(), // log in to system
-            "logout" => new UserLogout(self::$userPermission), // log out from system
-            "addBalanceAmount" => new AddUserBalance(self::$staffPermission), // add balance to user
-            "addQuantity" => new AddQuantity(self::$staffPermission), // add balance to user
-            "deleteBook" => new DeleteBook(self::$staffPermission), // delete a book
-            "addBook" => new AddBook(self::$staffPermission), // add a book
-            "addToBasket" => new AddToBasket(self::$userPermission), // add a book to basket
-            "deleteBasketItem" => new DeleteBasketItem(self::$userPermission), // remove a book from basket
-            "processBasket" => new ProcessBasket(self::$staffPermission), // process a user's basket
-            "deleteUser" => new DeleteUser(self::$staffPermission), // delete a user
-            "addCategory" => new AddCategory(self::$staffPermission), // add a new book category
-            "deleteCategory" => new DeleteCategory(self::$staffPermission) // delete a book category
+            "logout" => new ClassLoader("UserLogout", self::$userPermission), // log out from system
+            "addBalanceAmount" => new ClassLoader("AddUserBalance", self::$staffPermission), // add balance to user
+            "addQuantity" => new ClassLoader("AddQuantity", self::$staffPermission), // add balance to user
+            "deleteBook" => new ClassLoader("DeleteBook", self::$staffPermission), // delete a book
+            "addBook" => new ClassLoader("AddBook", self::$staffPermission), // add a book
+            "addToBasket" => new ClassLoader("AddToBasket", self::$userPermission), // add a book to basket
+            "deleteBasketItem" => new ClassLoader("DeleteBasketItem", self::$userPermission), // remove a book from basket
+            "processBasket" => new ClassLoader("ProcessBasket", self::$staffPermission), // process a user's basket
+            "deleteUser" => new ClassLoader("DeleteUser", self::$staffPermission), // delete a user
+            "addCategory" => new ClassLoader("AddCategory", self::$staffPermission), // add a new book category
+            "deleteCategory" => new ClassLoader("DeleteCategory", self::$staffPermission) // delete a book category
         );
     }
 
@@ -106,10 +108,16 @@ class ActionFactory {
      * @return IAction action
      */
     public function getAction($actionName) {
-        if (isset($this->actionMap[$actionName])) {
-            return $this->actionMap[$actionName];
-        } else {
+        if (!isset($this->actionMap[$actionName])) {
             throw new Exception("The action you are trying to run does not exist.");
+        }
+        
+        $action = $this->actionMap[$actionName];
+        
+        if($action instanceof ClassLoader) {
+            return $action->getClassInstance(); // temp workaround until I've structured this better
+        } else {
+            return $action;
         }
     }
 
